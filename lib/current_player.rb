df6d9e650162ef230a3f0ff1f315f4328b14db11
Samantha Balgobin <sbalgobin94@gmail.com>
2020-04-01 19:50:14 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,10 +1,9 @@
 def turn_count(board)
   counter = 0
   board.each do |element|
-    if element == "X" || element == "O"
+    if board[element == "X" || element == "O"
       counter += 1
       puts counter
-    end
   end
 end
 
