805831e562dcefbf8799dbfdae78418f99b5e1f5
Samantha Balgobin <sbalgobin94@gmail.com>
2020-04-01 19:13:02 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+def turn_count(board)
+  counter = 0
+  board.each do |element|
+    if element == "X" || element == "O"
+      counter += 1
+    end
+  end
+end
+
+def current_player(board)
+  turn_count(board)
+  if counter.even? == true
+    return "O"
+  end
+  if counter.odd? == true
+    return "X"
+  end
+end
